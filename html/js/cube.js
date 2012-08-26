b434c33b7368d750709f689a8a03886269c756e5
Norman Khine <khinester@aqoon.local>
2012-08-26 15:13:00 +0100

    created the cube in pure html

---
+++ html/js/cube.js
@@ -0,0 +1,19 @@
+﻿function set_all() {
+    $('#face_one').addClass('leftFace');
+    $('#face_two').addClass('topFace');
+    $('#face_three').addClass('rightFace');
+    $('#face_four').addClass('hidden');
+    $('#face_five').addClass('hidden');
+
+    $('.leftFace').click(function () {
+        $('#face_one').addClass('topFace').removeClass('leftFace');
+        $('#face_two').addClass('rightFace').removeClass('topFace');
+        $('#face_three').addClass('hidden').removeClass('rightFace');
+        $('#face_five').addClass('leftFace').removeClass('hidden');
+    });
+    
+    $('.topFace').click(function () {
+
+    });
+
+}
